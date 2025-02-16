cda2a60fa613d3d48be230775b7b3d70e3085051
HandsomeWeeb5 <farhantb2000@gmail.com>
2025-02-16 23:00:26 +0700

    Created special-price section for home page

---
+++ resources/views/components/special-price.blade.php
@@ -0,0 +1,9 @@
+<section class="section-display">
+    <div>
+        <h1>Special Price, Special Gadget</h1>
+        <p>You can get your dream gadget at a special price! we offer various exclusive discounts for the latest smartphones, laptops, and technology accessories. Don't miss out on limited offers and exciting promotions every day. Shop now and enjoy the best prices only at Super Gadget!</p>
+        <button>Go to Shop!</button>
+        <img src="{{ asset('img/women-smile-with-phone.jpg') }}" alt="women-smile-with-phone" class="w-25 block">
+        <img src="{{ asset('img/women-with-laptop-and-working.jpg') }}" alt="women-with-laptop-and-working" class="w-25 block">
+    </div>
+</section>
