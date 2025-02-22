6d698485f8603e53cf758a8e191c1ed218374307
Farhan <farhantb2000@gmail.com>
2025-02-22 23:48:17 +0700

    Fixed introduction view to make small object disappear when medium screen

--- resources/views/components/special-price.blade.php
+++ resources/views/components/special-price.blade.php
@@ -1,9 +1,15 @@
 <section class="section-display">
-    <div>
-        <h1>Special Price, Special Gadget</h1>
-        <p>You can get your dream gadget at a special price! we offer various exclusive discounts for the latest smartphones, laptops, and technology accessories. Don't miss out on limited offers and exciting promotions every day. Shop now and enjoy the best prices only at Super Gadget!</p>
-        <button>Go to Shop!</button>
-        <img src="{{ asset('img/women-smile-with-phone.jpg') }}" alt="women-smile-with-phone" class="w-25 block">
-        <img src="{{ asset('img/women-with-laptop-and-working.jpg') }}" alt="women-with-laptop-and-working" class="w-25 block">
+    <div class="grid grid-cols-2 grid-rows-2 gap-4">
+        <div class="row-span-2 flex justify-center flex-col gap-y-10 px-10">
+            <h1 class="text-7xl">Special Price, Special Gadget</h1>
+            <p class="text-lg">You can get your dream gadget at a special price! we offer various exclusive discounts for the latest smartphones, laptops, and technology accessories. Don't miss out on limited offers and exciting promotions every day. Shop now and enjoy the best prices only at Super Gadget!</p>
+            <button class="border bg-fuchsia-500 text-white font-bold px-5 py-2 rounded-lg max-w-fit mr-auto ml-auto">Go to Shop!</button>
+        </div>
+        <div class="grid place-items-center w-64">
+            <img src="{{ asset('img/women-smile-with-phone.jpg') }}" alt="women-smile-with-phone" class="object-fill">
+        </div>
+        <div class="grid place-items-center w-64">
+            <img src="{{ asset('img/women-with-laptop-and-working.jpg') }}" alt="women-with-laptop-and-working" class="relative top-0 left-56">
+        </div>
     </div>
 </section>
